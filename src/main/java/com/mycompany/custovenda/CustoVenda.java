f527f4080a104dc73cfdecbe0f8f5d89950e1099
Thaynan Ferreira <thaynan_ferreira@outlook.com.br>
2022-03-18 20:07:02 -0300

    Criaçao do programa

--- src/main/java/com/mycompany/custovenda/CustoVenda.java
+++ src/main/java/com/mycompany/custovenda/CustoVenda.java
@@ -4,11 +4,26 @@
  * and open the template in the editor.
  */
 package com.mycompany.custovenda;
-
+import java.util.Scanner;
 /**
  *
  * @author Thaynan
  */
 public class CustoVenda {
+    public static void main(String[] args) {
+        Scanner entrada = new Scanner(System.in);
+
+        double custo, margem, venda;
+        
+        System.out.println("Digite o prço de custo do Produto: ");
+        custo = entrada.nextDouble();
+        
+        System.out.println("Digite a margem de lucro desejada(%): ");
+        margem = entrada.nextDouble();
+        
+        venda = custo + (custo *(margem/100));
+        
+        System.out.println("Custo = " + custo + "\nVenda = " + venda + "\nMargem de lucro: " + margem + "%");
     
+    }
 }
